4c37c6c03a689a38fea3ab31ccd71f2120693259
vergarapog <201801166@iacademy.edu.ph>
2023-02-15 16:18:21 +0800

    added: semicolons, and eslint

--- exercise9_1_to_9_7/index.ts
+++ exercise9_1_to_9_7/index.ts
@@ -1,28 +1,28 @@
-import express from "express"
-import calculateBMI from "./bmiCalculator"
-const app = express()
+import express from "express";
+import calculateBMI from "./bmiCalculator";
+const app = express();
 
 app.get("/", (req, res) => {
-  console.log(req.body)
-  res.send("Hello FullStack!")
-})
+  console.log(req.body);
+  res.send("Hello FullStack!");
+});
 
 app.get("/bmi", (req, res) => {
-  const height = Number(req.query.height)
-  const weight = Number(req.query.weight)
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
 
   try {
-    const result = calculateBMI(height, weight)
-    return res.send(result)
+    const result = calculateBMI(height, weight);
+    return res.send(result);
   } catch (error: unknown) {
     return res.status(400).json({
       error: "malformatted parameters",
-    })
+    });
   }
-})
+});
 
-const PORT = 3002
+const PORT = 3002;
 
 app.listen(PORT, () => {
-  console.log("app running on port 3002")
-})
+  console.log("app running on port 3002");
+});
