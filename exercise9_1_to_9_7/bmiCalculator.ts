89fb9c3f56150bff5d9893a558d92f966e46e4ec
vergarapog <201801166@iacademy.edu.ph>
2023-02-15 17:33:58 +0800

    fixed: eslint errors

--- exercise9_1_to_9_7/bmiCalculator.ts
+++ exercise9_1_to_9_7/bmiCalculator.ts
@@ -1,69 +1,69 @@
 interface BMIInputValues {
-  value1: number
-  value2: number
+  value1: number;
+  value2: number;
 }
 
 const parseBmiArguments = (args: Array<string>): BMIInputValues => {
-  if (args.length < 4) throw new Error("not enough arguments")
-  if (args.length > 4) throw new Error("too much arguments")
+  if (args.length < 4) throw new Error("not enough arguments");
+  if (args.length > 4) throw new Error("too much arguments");
 
   if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
     return {
       value1: Number(args[2]),
       value2: Number(args[3]),
-    }
+    };
   } else {
-    throw new Error("Provided values were not numbers")
+    throw new Error("Provided values were not numbers");
   }
-}
+};
 
-const calculateBMI = (height: number, weight: number): String => {
-  let bmi: number = weight / ((height / 100) * (height / 100))
-  let bmiCategory
+const calculateBMI = (height: number, weight: number): string => {
+  const bmi: number = weight / ((height / 100) * (height / 100));
+  let bmiCategory;
 
   switch (true) {
     case bmi < 16:
-      bmiCategory = "Underweight (Severe thinness)"
-      break
+      bmiCategory = "Underweight (Severe thinness)";
+      break;
     case bmi >= 16 && bmi < 16.9:
-      bmiCategory = "Underweight (Moderate thinness)"
-      break
+      bmiCategory = "Underweight (Moderate thinness)";
+      break;
     case bmi >= 17 && bmi < 18.4:
-      bmiCategory = "Underweight (Mild thinness)"
-      break
+      bmiCategory = "Underweight (Mild thinness)";
+      break;
     case bmi >= 18.5 && bmi < 24.9:
-      bmiCategory = "Normal (healthy weight)"
-      break
+      bmiCategory = "Normal (healthy weight)";
+      break;
     case bmi >= 25 && bmi < 29.9:
-      bmiCategory = "Overweight (Pre-obese)"
-      break
+      bmiCategory = "Overweight (Pre-obese)";
+      break;
     case bmi >= 30 && bmi < 34.9:
-      bmiCategory = "Obese (Class I)"
-      break
+      bmiCategory = "Obese (Class I)";
+      break;
     case bmi >= 35 && bmi < 39.9:
-      bmiCategory = "Obese (Class II)"
-      break
+      bmiCategory = "Obese (Class II)";
+      break;
     case bmi >= 40:
-      bmiCategory = "Obese (Class III)"
-      break
+      bmiCategory = "Obese (Class III)";
+      break;
 
     default:
-      throw new Error("Provided values were not numbers")
+      throw new Error("Provided values were not numbers");
   }
 
-  return bmiCategory
-}
+  return bmiCategory;
+};
 
 try {
-  const { value1, value2 } = parseBmiArguments(process.argv)
+  const { value1, value2 } = parseBmiArguments(process.argv);
 
-  console.log(calculateBMI(value1, value2))
+  console.log(calculateBMI(value1, value2));
 } catch (error: unknown) {
-  let errorMessage = "Something has happened. "
+  let errorMessage = "Something has happened. ";
   if (error instanceof Error) {
-    errorMessage += "Error: " + error.message
+    errorMessage += "Error: " + error.message;
   }
-  console.log(errorMessage)
+  console.log(errorMessage);
 }
 
-export default calculateBMI
+export default calculateBMI;
--- exercise9_1_to_9_7/exerciseCalculator.ts
+++ exercise9_1_to_9_7/exerciseCalculator.ts
@@ -1,33 +1,33 @@
 interface Result {
-  periodLength: number
-  trainingDays: number
-  success: boolean
-  rating: string
-  ratingDescription: string
-  target: number
-  average: number
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: string;
+  ratingDescription: string;
+  target: number;
+  average: number;
 }
 
 interface Rating {
-  rating: string
-  ratingDescription: string
+  rating: string;
+  ratingDescription: string;
 }
 
 const parseExerciseArguments = (args: Array<string>): Array<number> => {
-  if (args.length < 4) throw new Error("not enough arguments")
+  if (args.length < 4) throw new Error("not enough arguments");
 
   const argsNum = args.slice(2, args.length).map((item) => {
     if (!isNaN(Number(item))) {
-      return Number(item)
+      return Number(item);
     } else {
-      throw new Error("All values should be numbers")
+      throw new Error("All values should be numbers");
     }
-  })
+  });
 
-  console.log(argsNum)
+  console.log(argsNum);
 
-  return argsNum
-}
+  return argsNum;
+};
 
 const computeRating = (average: number, target: number): Rating => {
   // let timeShortage = target - average
@@ -49,59 +49,59 @@ const computeRating = (average: number, target: number): Rating => {
   //   }
   // }
 
-  let timeShortage = target - average
-  let rating
-  let ratingDescription
+  const timeShortage = target - average;
+  let rating;
+  let ratingDescription;
 
   switch (true) {
     case average > target:
-      rating = "3 (highest)"
-      ratingDescription = "nigg thats good ngl"
+      rating = "3 (highest)";
+      ratingDescription = "nigg thats good ngl";
 
-      break
+      break;
 
     case timeShortage > 0.01 && timeShortage < 0.9:
-      rating = "2 (middle)"
-      ratingDescription = "kulang ng 5-55 minutes per day"
-      break
+      rating = "2 (middle)";
+      ratingDescription = "kulang ng 5-55 minutes per day";
+      break;
 
     case timeShortage > 1:
-      rating = "1 (lowest)"
-      ratingDescription = "kulang ng more than one hour per day"
-      break
+      rating = "1 (lowest)";
+      ratingDescription = "kulang ng more than one hour per day";
+      break;
 
     default:
-      rating = "0 (unknown)"
-      ratingDescription = "No rating was computed"
-      break
+      rating = "0 (unknown)";
+      ratingDescription = "No rating was computed";
+      break;
   }
 
   return {
     rating,
     ratingDescription,
-  }
-}
+  };
+};
 
 const calculateExercises = (
   daysPerHour: Array<number>,
   target: number
 ): Result => {
-  let periodLength = daysPerHour.length
+  const periodLength = daysPerHour.length;
 
-  let trainingDays = daysPerHour.reduce((accu, curr) => {
+  const trainingDays = daysPerHour.reduce((accu, curr) => {
     if (curr > 0) {
-      return accu + 1
+      return accu + 1;
     } else {
-      return accu
+      return accu;
     }
-  }, 0)
+  }, 0);
 
-  let average =
+  const average =
     daysPerHour.reduce((accu, curr) => {
-      return accu + curr
-    }, 0) / periodLength
+      return accu + curr;
+    }, 0) / periodLength;
 
-  let { rating, ratingDescription } = computeRating(average, target)
+  const { rating, ratingDescription } = computeRating(average, target);
 
   return {
     periodLength: periodLength,
@@ -111,16 +111,16 @@ const calculateExercises = (
     ratingDescription: ratingDescription,
     target: target,
     average: average,
-  }
-}
+  };
+};
 
 try {
-  const args = parseExerciseArguments(process.argv)
-  console.log(calculateExercises(args.slice(1, args.length), args[0]))
+  const args = parseExerciseArguments(process.argv);
+  console.log(calculateExercises(args.slice(1, args.length), args[0]));
 } catch (error: unknown) {
-  let errorMessage = "Something has gone wrong. "
+  let errorMessage = "Something has gone wrong. ";
   if (error instanceof Error) {
-    errorMessage += "Error: " + error.message
+    errorMessage += "Error: " + error.message;
   }
-  console.log(errorMessage)
+  console.log(errorMessage);
 }
