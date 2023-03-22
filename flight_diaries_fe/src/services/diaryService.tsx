3dfd8f299a5a350041fda338c205ac57e625b5ee
vergarapog <201801166@iacademy.edu.ph>
2023-03-22 18:45:32 +0800

    changed: .then service to await/async syntax

--- flight_diaries_fe/src/services/diaryService.tsx
+++ flight_diaries_fe/src/services/diaryService.tsx
@@ -3,12 +3,23 @@ import { Diary, NewDiary } from "../types";
 
 const baseUrl = "http://localhost:3001/api/diaries";
 
-const getAllDiaries = () => {
-  return axios.get<Diary[]>(baseUrl).then((res) => res.data);
+const getAllDiaries = async () => {
+  const res = await axios.get<Diary[]>(baseUrl);
+  return res.data;
 };
 
-const addDiary = (obj: NewDiary) => {
-  return axios.post<Diary[]>(baseUrl, obj).then((res) => res.data);
+const addDiary = async (obj: NewDiary) => {
+  const res = await axios.post<Diary[]>(baseUrl, obj);
+  return res.data;
 };
 
 export { getAllDiaries, addDiary };
+
+//.then way
+// const getAllDiaries = () => {
+//   return axios.get<Diary[]>(baseUrl).then((res) => res.data);
+// };
+
+// const addDiary = (obj: NewDiary) => {
+//   return axios.post<Diary[]>(baseUrl, obj).then((res) => res.data);
+// };
