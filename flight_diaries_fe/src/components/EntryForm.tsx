da176a58aa9e713e61df9e15187d038e6bf428e0
vergarapog <201801166@iacademy.edu.ph>
2023-03-24 17:17:48 +0800

    finished exe 9.19

--- flight_diaries_fe/src/components/EntryForm.tsx
+++ flight_diaries_fe/src/components/EntryForm.tsx
@@ -31,36 +31,127 @@ const EntryForm = (props: EntryFormProps) => {
   return (
     <div className="my-16" onSubmit={handleSubmit}>
       <h1 className="font-bold text-2xl">Add new entry</h1>
-      <form action="">
+      <form action="" className="space-y-3">
         <div className="space-x-2">
-          <label htmlFor="date">date:</label>
+          <label htmlFor="date">Date:</label>
           <input
             className="border"
             value={date}
             id="date"
-            type="text"
+            type="date"
             onChange={(e) => setDate(e.target.value)}
           />
         </div>
-        <div className="space-x-2">
-          <label htmlFor="visibility">visibility:</label>
-          <input
-            className="border"
-            value={visibility}
-            id="visibility"
-            type="text"
-            onChange={(e) => setVisibility(e.target.value)}
-          />
+        <div className="flex space-x-3">
+          <div>Visibility: </div>
+          <div className="space-x-2">
+            <div className="flex flex-col">
+              <div className="flex items-center space-x-3">
+                <input
+                  type="radio"
+                  id="great"
+                  name="visibility"
+                  value="great"
+                  onChange={(e) => setVisibility(e.target.value)}
+                />
+                <label htmlFor="great">Great</label>
+              </div>
+
+              <div className="flex items-center space-x-3">
+                <input
+                  type="radio"
+                  id="good"
+                  name="visibility"
+                  value="good"
+                  onChange={(e) => setVisibility(e.target.value)}
+                />
+                <label htmlFor="good">Good</label>
+              </div>
+
+              <div className="flex items-center space-x-3">
+                <input
+                  type="radio"
+                  id="ok"
+                  name="visibility"
+                  value="ok"
+                  onChange={(e) => setVisibility(e.target.value)}
+                />
+                <label htmlFor="ok">Ok</label>
+              </div>
+
+              <div className="flex items-center space-x-3">
+                <input
+                  type="radio"
+                  id="poor"
+                  name="visibility"
+                  value="poor"
+                  onChange={(e) => setVisibility(e.target.value)}
+                />
+                <label htmlFor="poor">Poor</label>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="space-x-2">
-          <label htmlFor="weather">weather:</label>
-          <input
-            className="border"
-            value={weather}
-            id="weather"
-            type="text"
-            onChange={(e) => setWeather(e.target.value)}
-          />
+
+        <div className="flex space-x-3">
+          <div>Weather: </div>
+          <div className="space-x-2">
+            <div className="flex flex-col">
+              <div className="flex items-center space-x-3">
+                <input
+                  type="radio"
+                  id="sunny"
+                  name="weather"
+                  value="sunny"
+                  onChange={(e) => setWeather(e.target.value)}
+                />
+                <label htmlFor="sunny">Sunny</label>
+              </div>
+
+              <div className="flex items-center space-x-3">
+                <input
+                  type="radio"
+                  id="rainy"
+                  name="weather"
+                  value="rainy"
+                  onChange={(e) => setWeather(e.target.value)}
+                />
+                <label htmlFor="rainy">Rainy</label>
+              </div>
+
+              <div className="flex items-center space-x-3">
+                <input
+                  type="radio"
+                  id="cloudy"
+                  name="weather"
+                  value="cloudy"
+                  onChange={(e) => setWeather(e.target.value)}
+                />
+                <label htmlFor="cloudy">Cloudy</label>
+              </div>
+
+              <div className="flex items-center space-x-3">
+                <input
+                  type="radio"
+                  id="stormy"
+                  name="weather"
+                  value="stormy"
+                  onChange={(e) => setWeather(e.target.value)}
+                />
+                <label htmlFor="stormy">Stormy</label>
+              </div>
+              <div className="flex items-center space-x-3">
+                <input
+                  type="radio"
+                  id="windy"
+                  name="weather"
+                  value="windy"
+                  onChange={(e) => setWeather(e.target.value)}
+                />
+                <label htmlFor="windy">Windy</label>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="space-x-2">
           <label htmlFor="comment">comment:</label>
