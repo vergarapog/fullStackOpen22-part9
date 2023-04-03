73e756b826e558df8fd42b99bd5109b347976cec
vergarapog <201801166@iacademy.edu.ph>
2023-04-03 18:45:21 +0800

    patientor_fe: finished exe 9.23

--- patientor/src/components/SinglePatientPage/index.tsx
+++ patientor/src/components/SinglePatientPage/index.tsx
@@ -7,14 +7,36 @@ interface SinglePatientPageProps {
 }
 
 const SinglePatientPage = ({ singlePatient }: SinglePatientPageProps) => {
+  const { name, gender, ssn, occupation, entries } = singlePatient || {};
+
   return singlePatient ? (
     <div>
       <div>
-        <h1>{singlePatient.name}</h1>
-        <div>{singlePatient.gender === "male" ? <Male /> : <Female />}</div>
+        <h1>{name}</h1>
+        <div>{gender === "male" ? <Male /> : <Female />}</div>
       </div>
-      <p>ssn: {singlePatient.ssn}</p>
-      <p>occupation: {singlePatient.occupation}</p>
+      <p>ssn: {ssn}</p>
+      <p>occupation: {occupation}</p>
+
+      {entries && entries.length ? (
+        <div>
+          <h1>entries</h1>
+          {entries.map((entry) => {
+            return (
+              <div>
+                {entry.date} {entry.description}
+                <ul>
+                  {entry.diagnosisCodes?.map((diagnosisCode) => {
+                    return <li>{diagnosisCode}</li>;
+                  })}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div>no entries available</div>
+      )}
     </div>
   ) : (
     <div>Patient not found</div>
--- patientor/src/types.ts
+++ patientor/src/types.ts
@@ -1,4 +1,4 @@
-export interface Diagnosis {
+export interface Diagnose {
   code: string;
   name: string;
   latin?: string;
@@ -7,7 +7,7 @@ export interface Diagnosis {
 export enum Gender {
   Male = "male",
   Female = "female",
-  Other = "other"
+  Other = "other",
 }
 
 export interface Patient {
@@ -16,7 +16,44 @@ export interface Patient {
   occupation: string;
   gender: Gender;
   ssn?: string;
+  entries: Array<Entry>;
   dateOfBirth?: string;
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
+
+export type Entry = HospitalEntry | OccupationalHealthEntry | HealthCheckEntry;
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose["code"]>;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: { date: string; criteria: string };
+}
+
+export interface OccupationalHealthEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
